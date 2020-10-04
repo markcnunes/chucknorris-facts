ecf0ebe7520b0d82981908a0d206d712892b4cd1
Mark Nunes <mark.nunes@asp.events>
2020-10-04 11:23:55 +0100

    change GIPHY API to https

--- src/httpRequest/fetchGif.js
+++ src/httpRequest/fetchGif.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchGif = async () => {
   const offset = Math.floor(Math.random() * 60);
-  const API = `http://api.giphy.com/v1/gifs/search?q=chuck+norris&api_key=${process.env.REACT_APP_GIPHY}&offset=${offset}&limit=1`;
+  const API = `https://api.giphy.com/v1/gifs/search?q=chuck+norris&api_key=${process.env.REACT_APP_GIPHY}&offset=${offset}&limit=1`;
 
   return await axios.get(API);
 };
